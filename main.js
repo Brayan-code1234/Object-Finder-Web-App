8e4904fa969a0b5f34e2c5081173301d56627ad2
Brayan-code1234 <77586737+Brayan-code1234@users.noreply.github.com>
2022-02-26 12:03:05 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+status = "";
+
+
+
+function setup()
+{
+   canvas = createCanvas(480, 380);
+   canvas.center();
+   video = createCapture(VIDEO);
+   video.size(480, 380);
+   video.hide();
+}
+
+function start(){
+   objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+   document.getElementById("status").innerHTML = "detecting objects";
+}
+
+function modelLoaded(){
+   status = true;
+}
+
+function draw(){
+  image(video, 0, 0, 480, 380);
+}
